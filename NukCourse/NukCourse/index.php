1bc2d2dc1c425dc2ddc8878a5762fd4f60983773
Bill Huang <bill.huang@jetfusion.com.tw>
2017-05-03 22:54:18 +0800

    First project for nuk

---
+++ NukCourse/NukCourse/index.php
@@ -0,0 +1,42 @@
+<?php
+
+echo "Hello World!";
+print_r(array('A' => 'Apple'));
+
+function foo($bar)
+{
+        // ...
+}
+
+foreach ($arr as $key => $val)
+{
+        // ...
+}
+
+if ($foo == $bar)
+{
+        // ...
+}
+else
+{
+        // ...
+}
+
+for ($i = 0; $i < 10; $i++)
+{
+        for ($j = 0; $j < 10; $j++)
+        {
+                // ...
+        }
+}
+
+try
+{
+        // ...
+}
+catch()
+{
+        // ...
+}
+
+?>
